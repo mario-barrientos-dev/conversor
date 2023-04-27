ddcbb950b0ca029a54c3a42ae874a0fb24db91da
mbarrientos.externos <mariobarrientos0303@gmail.com>
2023-04-27 09:19:32 -0500

    'Ajuste Footer'

--- conversor/conversor_excel.py
+++ conversor/conversor_excel.py
@@ -6,28 +6,23 @@ def is_empty_dataframe(df):
     return df.empty or all(df.dropna(axis=1, how='all').dropna(axis=0, how='all').columns.isnull())
 
 def extraer_tablas_pdf(archivo_pdf: str) -> list:
-    """ 
-    `archivo_pdf`: una cadena que contiene el nombre del archivo PDF que se va a procesar.
-    `tablas`: una lista de DataFrames que contienen las tablas extraídas del PDF.
-    Esta función utiliza la biblioteca tabula para extraer todas las tablas de un archivo PDF y devuelve una lista de DataFrames que representan las tablas extraídas.
-    """
     tablas = []
     with pdfplumber.open(archivo_pdf) as pdf:
         for page in pdf.pages:
             # Primero intenta extraer la tabla sin estrategia
             tabla = page.extract_table()
-            
+            print(tabla)
             if tabla:
                 df = pd.DataFrame(tabla[1:], columns=tabla[0])
                 print('Aqui esta el df ' + str(df))
                 if is_empty_dataframe(df):
-                    tablas = tabula.read_pdf(archivo_pdf, pages='all', multiple_tables=True, encoding='latin1', stream=True, guess=False)
+                    tablas = tabula.read_pdf(archivo_pdf, pages='all', multiple_tables=True, encoding='latin1', stream=True, guess=False, lattice=True, area=None, relative_area=True, columns=None, split_text=True)
                     print("Aqui entro vacio")
                     print(tablas)
                     return tablas
                 else:
                     print("Aqui entro lleno")
-                    tablas = tabula.read_pdf(archivo_pdf, pages='all', multiple_tables=True, encoding='latin1')
+                    tablas = tabula.read_pdf(archivo_pdf, pages='all', multiple_tables=True, encoding='latin1', lattice=True, area=None, relative_area=True, columns=None, split_text=True)
     print(tablas)
     return tablas
 
@@ -54,4 +49,5 @@ def guardar_en_excel(datos, nombre_archivo_salida):
     with pd.ExcelWriter(nombre_archivo_salida, engine='openpyxl') as writer:
         for i, tabla in enumerate(datos):
             tabla.to_excel(writer, index=False, sheet_name=f'Hoja{i+1}')
+        
         writer.book.save(nombre_archivo_salida)
